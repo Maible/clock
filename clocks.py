6fdd5d728544d880a81464de5c15a9113e5a9b38
Emin Mastizada <emin@linux.com>
2019-12-27 01:08:32 +0300

    Optimize, make info rectangle with radius, add an example button

--- clocks.py
+++ clocks.py
@@ -2,71 +2,70 @@ import math
 import os
 from datetime import date
 
-from PyQt5.QtCore import QPoint, Qt, QDateTime, QTime, QTimer, QSettings, QRect, QRectF
-from PyQt5.QtCore import QCoreApplication
-from PyQt5.QtGui import QColor, QPainter, QPolygon, QIcon, QFont, QPen, QBrush, QPainterPath
-from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QDialog
+from PyQt5 import QtGui, QtCore
+from PyQt5.QtWidgets import QMainWindow, QPushButton, QSizePolicy
+
 from settingsform import SettingsDialog
 
 
 class AppClock(QMainWindow):
-    hourHand = QPolygon([
-        QPoint(4, 8),
-        QPoint(-4, 8),
-        QPoint(-4, -40),
-        QPoint(4, -40)
+    hourHand = QtGui.QPolygon([
+        QtCore.QPoint(4, 8),
+        QtCore.QPoint(-4, 8),
+        QtCore.QPoint(-4, -40),
+        QtCore.QPoint(4, -40)
     ])
-    minuteHand = QPolygon([
-        QPoint(3, 8),
-        QPoint(-2, 8),
-        QPoint(-2, -60),
-        QPoint(3, -60)
+    minuteHand = QtGui.QPolygon([
+        QtCore.QPoint(3, 8),
+        QtCore.QPoint(-2, 8),
+        QtCore.QPoint(-2, -60),
+        QtCore.QPoint(3, -60)
     ])
-    secondHand = QPolygon([
-        QPoint(3, 8),
-        QPoint(-3, 8),
-        QPoint(-3, -70),
-        QPoint(3, -70)
+    secondHand = QtGui.QPolygon([
+        QtCore.QPoint(3, 8),
+        QtCore.QPoint(-3, 8),
+        QtCore.QPoint(-3, -70),
+        QtCore.QPoint(3, -70)
     ])
 
     def updateClock(self):
-        time = QTime.currentTime()
+        time = QtCore.QTime().currentTime()
         # update every 10 second
         # if time.second() % 10 == 0:
         #     self.update()
         self.update()
 
-    def setWindowFrame(self, withFrame):
-        self.withFrame = withFrame
+    def setWindowFrame(self, with_frame):
+        self.withFrame = with_frame
         flags = self.windowFlags()
-        if not withFrame:
-            flags |= Qt.WindowStaysOnTopHint | Qt.FramelessWindowHint
+        if not with_frame:
+            flags |= QtCore.Qt.WindowStaysOnTopHint | QtCore.Qt.FramelessWindowHint
         else:
-            flags &= ~(Qt.WindowStaysOnBottomHint | Qt.FramelessWindowHint)
+            flags &= ~(QtCore.Qt.WindowStaysOnBottomHint | QtCore.Qt.FramelessWindowHint)
         self.setWindowFlags(flags)
 
     def setup_main_settings(self, app_settings):
-        self.smokeBackgroundColor = QColor(*app_settings.background_color)
-        self.hourColor = QColor(*app_settings.hour_color)
-        self.minuteColor = QColor(*app_settings.minute_color)
-        self.secondColor = QColor(*app_settings.second_color)
-        self.whiteShadowColor = QColor(*app_settings.shadow_color)
-        self.helperColor = QColor(*app_settings.helper_color)
-        self.textColor = QColor(*app_settings.text_color)
-        self.textPanelColor = QColor(*app_settings.helper_text_color)
+        self.smokeBackgroundColor = QtGui.QColor(*app_settings.background_color)
+        self.hourColor = QtGui.QColor(*app_settings.hour_color)
+        self.minuteColor = QtGui.QColor(*app_settings.minute_color)
+        self.secondColor = QtGui.QColor(*app_settings.second_color)
+        self.whiteShadowColor = QtGui.QColor(*app_settings.shadow_color)
+        self.helperColor = QtGui.QColor(*app_settings.helper_color)
+        self.textColor = QtGui.QColor(*app_settings.text_color)
+        self.textPanelColor = QtGui.QColor(*app_settings.helper_text_color)
 
     def __init__(self, parent=None, withFrame=False, app_settings=None):
         super().__init__(parent)
         self.app_settings = app_settings
         self.setup_main_settings(self.app_settings)
         # window title and icon
-        self.setWindowIcon(QIcon(os.path.join(app_settings.images_dir, "icon.png")))
-        self.setAttribute(Qt.WA_TranslucentBackground)
+        self.setWindowIcon(QtGui.QIcon(os.path.join(app_settings.images_dir, "icon.png")))
+        self.setAttribute(QtCore.Qt.WA_TranslucentBackground)
         # app settings
-        self.settings = QSettings(app_settings.app_authors, app_settings.app_name)
+        self.settings = QtCore.QSettings(app_settings.app_authors, app_settings.app_name)
 
         # initialize QtTimer
-        timer = QTimer(self)
+        timer = QtCore.QTimer(self)
         timer.timeout.connect(self.updateClock)
         timer.start(1000)
 
@@ -81,17 +80,51 @@ class AppClock(QMainWindow):
         # set main window name
         self.setWindowTitle(app_settings.app_name)
         # set font
-        font = QFont()
-        font.setStyleHint(QFont.SansSerif)
+        font = QtGui.QFont()
+        font.setStyleHint(QtGui.QFont.SansSerif)
         font.setFamily('monospace')
         font.setPointSize(12)
         self.font = font
-        font = QFont(font)
+        font = QtGui.QFont(font)
         font.setPointSize(13)
         self.helperFont = font
-        font = QFont(font)
+        font = QtGui.QFont(font)
         font.setPointSize(10)
         self.dailyFont = font
+
+        size_policy = QSizePolicy(QSizePolicy.Preferred, QSizePolicy.Preferred)
+        size_policy.setHeightForWidth(True)
+        self.setSizePolicy(size_policy)
+
+        # schedule buttons
+        # button style
+        btn_style = """
+                    background-color: rgba(0, 0, 0, 0.1);
+                    border-radius: 1px;
+                    color: transparent;
+                    """
+        # place buttons
+        width_factor = self.width() / 200
+        # place buttons
+        self.events_btn_1 = QPushButton('TestBtn', self)
+        self.events_btn_1.resize(width_factor, width_factor)
+        self.events_btn_1.move(width_factor * 110, width_factor * 10)
+        self.events_btn_1.setToolTip("Some btn")
+        self.events_btn_1.setStyleSheet(btn_style)
+
+    def resizeEvent(self, event):
+        size = event.size()
+        # import code
+        # code.interact(local=locals())
+        if event.oldSize().width() != size.width():
+            # width changed
+            size.setHeight(size.width())
+            super().resizeEvent(QtGui.QResizeEvent(size, event.oldSize()))
+        else:
+            # height changed
+            size.setWidth(size.height())
+            super().resizeEvent(QtGui.QResizeEvent(size, event.oldSize()))
+        self.resize(size.width(), size.height())
 
     def rotatedPoint(self, x, y, degr):
         theta = degr * math.pi / 180
@@ -101,52 +134,52 @@ class AppClock(QMainWindow):
 
     def paint_analog_clock(self, event):
         side = min(self.width(), self.height())
-        timeDate = QDateTime.currentDateTime()
+        timeDate = QtCore.QDateTime.currentDateTime()
         timeDateStr = timeDate.toString("HH:mm\nd MMM")
         time = timeDate.time()
         isAm = time.hour() < 12
 
-        whiteShadowPen = QPen(self.whiteShadowColor)
-        whiteShadowPen.setJoinStyle(Qt.MiterJoin)
+        whiteShadowPen = QtGui.QPen(self.whiteShadowColor)
+        whiteShadowPen.setJoinStyle(QtCore.Qt.MiterJoin)
         whiteShadowPen.setWidthF(0.9)
 
         y0 = -90 if 15 <= time.minute() < 45 else 20
         x0 = -90 if 0 <= time.hour() % 12 < 6 else 20
-        textPanelRect = QRectF(x0, y0, 69, 20)
+        textPanelRect = QtCore.QRectF(x0, y0, 69, 20)
 
-        painter = QPainter()
+        painter = QtGui.QPainter()
         painter.begin(self)
-        painter.setRenderHint(QPainter.Antialiasing)
+        painter.setRenderHint(QtGui.QPainter.Antialiasing)
         painter.translate(self.width() / 2, self.height() / 2)
         painter.scale(side / 200.0, side / 200.0)
 
         # draw clock frame
         painter.setClipping(True)
-        p = QPainterPath()
-        p.addRect(QRectF(-100, -100, 200, 200))
-        p2 = QPainterPath()
-        p2.addRect(QRectF(textPanelRect))
+        p = QtGui.QPainterPath()
+        p.addRect(QtCore.QRectF(-100, -100, 200, 200))
+        p2 = QtGui.QPainterPath()
+        p2.addRect(QtCore.QRectF(textPanelRect))
         p = p.subtracted(p2)
         painter.setClipPath(p)
 
         painter.setPen(whiteShadowPen)
-        painter.setBrush(QBrush(self.smokeBackgroundColor))
-        painter.drawEllipse(QPoint(0, 0), 99, 99)
+        painter.setBrush(QtGui.QBrush(self.smokeBackgroundColor))
+        painter.drawEllipse(QtCore.QPoint(0, 0), 99, 99)
 
         # draw hours
         painter.setPen(whiteShadowPen)
         painter.setFont(self.helperFont)
-        painter.setBrush(QBrush(self.hourColor))
+        painter.setBrush(QtGui.QBrush(self.hourColor))
         for i in range(0, 12):
             x, y = self.rotatedPoint(0, -92, i * 360/12)
             painter.drawEllipse(x - 3, y - 3, 6, 6)
         painter.setPen(self.helperColor)
         for i in range(0, 12):
             x, y = self.rotatedPoint(0, -76, i * 360/12)
-            painter.drawText(QRect(x - 10, y - 10, 20, 20), Qt.AlignCenter, "%d" % (i if isAm else i + 12))
+            painter.drawText(QtCore.QRect(x - 10, y - 10, 20, 20), QtCore.Qt.AlignCenter, "%d" % (i if isAm else i + 12))
 
         painter.setPen(whiteShadowPen)
-        painter.setBrush(QBrush(self.minuteColor))
+        painter.setBrush(QtGui.QBrush(self.minuteColor))
         for j in range(0, 60):
             if j % 5 != 0:
                 x, y = self.rotatedPoint(0, -92, j * 360/60)
@@ -155,31 +188,31 @@ class AppClock(QMainWindow):
 
         # draw digital clock panel
         painter.setPen(whiteShadowPen)
-        painter.setBrush(QBrush(self.textPanelColor))
+        painter.setBrush(QtGui.QBrush(self.textPanelColor))
         painter.drawRect(textPanelRect)
         texts = timeDateStr.split('\n')
         painter.setFont(self.font)
         painter.setPen(self.textColor)
         h2 = textPanelRect.height() / 1
         # hour
-        # rect = QRect(textPanelRect.left(), textPanelRect.top() + 5, textPanelRect.width(), h2-5)
+        # rect = QtCore.QRect(textPanelRect.left(), textPanelRect.top() + 5, textPanelRect.width(), h2-5)
         # painter.drawText(rect, Qt.AlignCenter, texts[0])
         # date
-        rect = QRect(textPanelRect.left(), textPanelRect.top(), textPanelRect.width(), h2-1)
-        painter.drawText(rect, Qt.AlignCenter, texts[1])
+        rect = QtCore.QRect(textPanelRect.left(), textPanelRect.top(), textPanelRect.width(), h2-1)
+        painter.drawText(rect, QtCore.Qt.AlignCenter, texts[1])
 
         # hour pointer
         painter.setPen(whiteShadowPen)
-        painter.setBrush(QBrush(self.hourColor))
+        painter.setBrush(QtGui.QBrush(self.hourColor))
 
         painter.save()
-        painter.rotate(30.0 * ((time.hour() + time.minute() / 60.0)))
+        painter.rotate(30.0 * (time.hour() + time.minute() / 60.0))
         painter.drawConvexPolygon(self.hourHand)
         painter.restore()
 
         # minute pointer
         painter.setPen(whiteShadowPen)
-        painter.setBrush(QBrush(self.minuteColor))
+        painter.setBrush(QtGui.QBrush(self.minuteColor))
 
         painter.save()
         painter.rotate(6.0 * (time.minute() + time.second() / 60.0))
@@ -188,7 +221,7 @@ class AppClock(QMainWindow):
 
         # second pointer
         painter.setPen(whiteShadowPen)
-        painter.setBrush(QBrush(self.secondColor))
+        painter.setBrush(QtGui.QBrush(self.secondColor))
 
         painter.save()
         painter.rotate(6.0 * (time.second()))
@@ -199,76 +232,76 @@ class AppClock(QMainWindow):
 
     def paint_weekday_clock(self, event):
         # generate localized days
+        timer = QtCore.QDateTime()
         days = []
-        base_date = QDateTime.currentDateTime()
+        base_date = timer.currentDateTime()
         for i in range(7):
-            QDateTime.setDate(base_date, date(year=2019, month=4, day=i+1))
+            QtCore.QDateTime.setDate(base_date, date(year=2019, month=4, day=i+1))
             days.append(base_date.toString("ddd"))
 
         # days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
         side = min(self.width(), self.height())
-        timeDate = QDateTime.currentDateTime()
+        timeDate = timer.currentDateTime()
         timeDateStr = timeDate.toString("HH:mm\ndd MMM")
-        time = timeDate.time()
 
-        whiteShadowPen = QPen(self.whiteShadowColor)
-        whiteShadowPen.setJoinStyle(Qt.MiterJoin)
+        whiteShadowPen = QtGui.QPen(self.whiteShadowColor)
+        whiteShadowPen.setJoinStyle(QtCore.Qt.MiterJoin)
         whiteShadowPen.setWidthF(0.9)
 
         current_day_index = days.index(timeDate.toString("ddd"))
-        y0 = -90 if 0 <= current_day_index < 3 else 20
-        x0 = -90 if 0 <= current_day_index < 3 else 20
-        textPanelRect = QRectF(x0, y0, 69, 40)
+        y0 = -90 if 0 <= current_day_index < 4 else 20
+        x0 = -90 if 0 <= current_day_index < 4 else 20
+        textPanelRect = QtCore.QRectF(x0, y0, 69, 40)
 
-        painter = QPainter()
+        painter = QtGui.QPainter()
         painter.begin(self)
-        painter.setRenderHint(QPainter.Antialiasing)
+        painter.setRenderHint(QtGui.QPainter.Antialiasing)
         painter.translate(self.width() / 2, self.height() / 2)
         painter.scale(side / 200.0, side / 200.0)
 
         # draw clock frame
         painter.setClipping(True)
-        p = QPainterPath()
-        p.addRect(QRectF(-100, -100, 200, 200))
-        p2 = QPainterPath()
-        p2.addRect(QRectF(textPanelRect))
+        p = QtGui.QPainterPath()
+        p.addRoundedRect(QtCore.QRectF(-100, -100, 200, 200), 10, 10)
+        p2 = QtGui.QPainterPath()
+        p2.addRoundedRect(QtCore.QRectF(textPanelRect), 10, 10)
         p = p.subtracted(p2)
         painter.setClipPath(p)
 
         painter.setPen(whiteShadowPen)
-        painter.setBrush(QBrush(self.smokeBackgroundColor))
-        painter.drawEllipse(QPoint(0, 0), 99, 99)
+        painter.setBrush(QtGui.QBrush(self.smokeBackgroundColor))
+        painter.drawEllipse(QtCore.QPoint(0, 0), 99, 99)
 
         # draw days
         painter.setPen(whiteShadowPen)
         painter.setFont(self.dailyFont)
-        painter.setBrush(QBrush(self.hourColor))
+        painter.setBrush(QtGui.QBrush(self.hourColor))
         for i in range(0, 7):
             x, y = self.rotatedPoint(0, -92, i * 360/7)
             painter.drawEllipse(x - 3, y - 3, 6, 6)
         painter.setPen(self.helperColor)
         for i in days:
             x, y = self.rotatedPoint(0, -76, days.index(i) * 360/7)
-            painter.drawText(QRect(x - 10, y - 10, 30, 20), Qt.AlignCenter, "%s" % i)
+            painter.drawText(QtCore.QRect(x - 10, y - 10, 30, 20), QtCore.Qt.AlignCenter, "%s" % i)
 
         painter.setClipping(False)
 
         painter.setPen(whiteShadowPen)
-        painter.setBrush(QBrush(self.textPanelColor))
-        painter.drawRect(textPanelRect)
+        painter.setBrush(QtGui.QBrush(self.textPanelColor))
+        painter.drawRoundedRect(textPanelRect, 10, 10)
         texts = timeDateStr.split('\n')
         painter.setFont(self.font)
         painter.setPen(self.textColor)
         h2 = textPanelRect.height() / 2
 
-        rect = QRect(textPanelRect.left(), textPanelRect.top(), textPanelRect.width(), h2-1)
-        painter.drawText(rect, Qt.AlignCenter, texts[0])
+        rect = QtCore.QRect(textPanelRect.left(), textPanelRect.top(), textPanelRect.width(), h2-1)
+        painter.drawText(rect, QtCore.Qt.AlignCenter, texts[0])
         # date
-        rect = QRect(textPanelRect.left(), textPanelRect.top() + 18, textPanelRect.width(), h2-1)
-        painter.drawText(rect, Qt.AlignCenter, texts[1])
+        rect = QtCore.QRect(textPanelRect.left(), textPanelRect.top() + 18, textPanelRect.width(), h2-1)
+        painter.drawText(rect, QtCore.Qt.AlignCenter, texts[1])
 
         painter.setPen(whiteShadowPen)
-        painter.setBrush(QBrush(self.hourColor))
+        painter.setBrush(QtGui.QBrush(self.hourColor))
 
         painter.save()
         painter.rotate(360.0/7 * current_day_index)
@@ -282,6 +315,12 @@ class AppClock(QMainWindow):
             self.paint_analog_clock(event)
         else:
             self.paint_weekday_clock(event)
+
+        width_factor = self.width() / 100
+        height_factor = self.height() / 100
+        # place buttons
+        self.events_btn_1.resize(width_factor*10, width_factor*10)
+        self.events_btn_1.move(width_factor * 50, height_factor * 3)
 
     def settings_window(self):
         dialog = SettingsDialog(self, app_settings=self.app_settings)
